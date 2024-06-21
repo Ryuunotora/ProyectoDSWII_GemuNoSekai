4eb8e2b7cf2d8c6ff2fa454f6c642acb3bc3191f
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-06-21 17:42:40 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/cibertec/ProyectoDSWII_GemuNoSekai/ProyectoDswiiGemuNoSekaiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ProyectoDSWII_GemuNoSekai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDswiiGemuNoSekaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDswiiGemuNoSekaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/ProyectoDSWII_GemuNoSekai/ProyectoDswiiGemuNoSekaiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ProyectoDSWII_GemuNoSekai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDswiiGemuNoSekaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
